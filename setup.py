50c0919bf3b7c293daa3582c09faa4a4230dc8c4
alonsocanov <45244628+alonsocanov@users.noreply.github.com>
2024-08-13 09:59:34 -0400

    updated library requirements

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
-from gettext import install
 from setuptools import setup, find_packages
-import os
+
 
 setup(
     name='amelia_inference',
-    packages=find_packages(['./tools/*'], exclude=['test*']),
+    packages=find_packages(['./amelia_inference/*']),
     version='1.0.0',
-    description="Tool runing amelia's framework.",
+    description="Tool running amelia's framework",
+    url='https://github.com/AmeliaCMU/AmeliaInference',
     install_requires=[
-        # "src @ ../amelia_tf",
+        'python-dateutil==2.9.0',
+        'numpy==1.21.2',
+        'matplotlib==3.7.1',
+        'imageio==2.34.0',
+        'torch==2.4.0',
+        'tqdm==4.65.0',
+        'hydra-core==1.3.2',
+        'hydra_colorlog==1.2.0',
+        'contextily==1.3.0',
+        'easydict==1.10',
+        'opencv-python==4.7.0.72',
     ],
 )
